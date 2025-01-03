47c03a4fb4a2fa87d68a8639f504faf63fef5642
yeni_bababatunde <babatundeabike3@gmail.com>
2025-01-03 11:10:00 +0100

    spm_package_removed

--- GithubProfiles/App/AppDelegate.swift
+++ GithubProfiles/App/AppDelegate.swift
@@ -6,21 +6,14 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        handleKeyboardDismissal()
         return true
     }
-
-    func handleKeyboardDismissal() {
-        IQKeyboardManager.shared.enable = true
-        IQKeyboardManager.shared.keyboardAppearance = .default
-        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
-    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {

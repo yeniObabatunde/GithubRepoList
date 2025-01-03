922592fa7a4cd64f8a03aec9833264d9235e2ad6
yeni_bababatunde <babatundeabike3@gmail.com>
2025-01-03 10:11:52 +0100

    Create Logger.swift

---
+++ GithubProfiles/Utils/Logger.swift
@@ -0,0 +1,31 @@
+//
+//  Logger.swift
+//  GithubProfiles
+//
+//  Created by Sharon Omoyeni Babatunde on 01/01/2025.
+//
+
+import Foundation
+
+enum LogType {
+    case success
+    case error
+    case info
+}
+
+class Logger {
+    
+    static func printIfDebug(data: String, logType: LogType) {
+#if DEBUG
+        switch logType {
+        case .success:
+            print("🟢🟢🟢", data)
+        case .error:
+            print("🛑🛑🛑", data)
+        case .info:
+            print("🟡🟡🟡", data)
+        }
+#endif
+    }
+    
+}
